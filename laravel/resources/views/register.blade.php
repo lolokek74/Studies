512aa390dca1e263c24fba09a8516ab43da6fab0
is41-rodenkoav <1>
2022-09-15 11:39:05 +0500

    v1.0.4 Site 70%

--- laravel/resources/views/register.blade.php
+++ laravel/resources/views/register.blade.php
@@ -1,27 +1,42 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Страница регистрации</title>
-</head>
-<body>
-<form action="{{ route('register') }}" method="POST">
-    @if(session()->has('success'))
-        <h3>Операция регистрации успешно выполнена !</h3>
-        @endif
-        @csrf
-        <input type="text" name="name" placeholder="Ваше имя:"><br>
-        @error('name')<p>{{ $message }}</p>@enderror
-        <input type="email" name="email" placeholder="Ваша почта:"><br>
-        @error('email')<p>{{ $message }}</p>@enderror
-        <input type="password" name="password" placeholder="Ваш пароль:"><br>
-        @error('password')<p>{{ $message }}</p>@enderror
-        <input type="password" name="password_confirmation" placeholder="Ваш пароль повторно:"><br>
-        @error('password')<p>{{ $message }}</p>@enderror
-        <input type="submit" value="Регистрация">
-</form>
-</body>
-</html>
+@extends('index')
+
+@section('title', 'Страница авторизации')
+
+@section('content')
+    <div class="container p-4">
+        <div class="row">
+            <div class="col"></div>
+            <div class="col-6">
+                <form action="{{ route('register') }}" method="POST">
+                    @if(session()->has('success'))
+                        <h3>Операция регистрации успешно выполнена !</h3>
+                    @endif
+                    @csrf
+                        <div class="mb-3">
+                            <label for="inputName" class="form-label">Ваше имя:</label>
+                            <input type="text" class="form-control" id="inputName" name="name">
+
+                        </div>
+                        <div class="mb-3">
+                            <label for="inputEmail" class="form-label">Ваша почта:</label>
+                            <input type="email" class="form-control" id="inputEmail" name="email">
+                        </div>
+                        <div class="mb-3">
+                            <label for="inputPassword" class="form-label">Ваша пароль:</label>
+                            <input type="password" class="form-control" id="inputPassword" name="password">
+                        </div>
+                        <div class="mb-3">
+                            <label for="inputPasswordConfirmation" class="form-label">Ваша пароль повторно:</label>
+                            <input type="password" class="form-control" id="inputPasswordConfirmation" name="password_confirmation" aria-describedby="invalidPasswordConfirmation">
+                            <div id="invalidPasswordConfirmation" class="invalid-feedback">
+                                Please choose a username.
+                            </div>
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">Регистрация</button>
+                </form>
+            </div>
+            <div class="col"></div>
+        </div>
+    </div>
+@endsection
